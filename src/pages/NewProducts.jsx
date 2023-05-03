ebbf24fc991d0d9bb57902d53a32a7e3a45c35b5
joannashin <joannashin3@gmail.com>
2023-05-03 21:22:46 +0900

    상품 등록페이지 작업

--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -1,8 +1,89 @@
+import React, { useState } from 'react';
+import Button from '../components/ui/Button';
+
 function NewProduct() {
+    const [product, setProduct] = useState({});
+    const [file, setFile] = useState();
+    
+    const handleChange = (e) => {
+        const {name, value, files} = e.target;
+
+        if (name === 'file') {
+            setFile(files && files[0])
+            return;
+        }
+        setProduct((product) => ({...product, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+    }
+
     return (
-        <>
-            NewProduct
-        </>
+        <section>
+            {file && <img src={URL.createObjectURL(file)} alt='local file'/>}
+            <h2>new product</h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="file" 
+                    accept='image/*'
+                    name="file"
+                    required 
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    name="title"
+                    value={product.title ?? ''}
+                    placeholder='제품명'
+                    required
+                    onChange={handleChange}
+                />
+                <input 
+                    type="number" 
+                    name="price"
+                    value={product.price ?? ''}
+                    placeholder='가격'
+                    required
+                    onChange={handleChange}
+                />
+                <input 
+                    type="number" 
+                    name="price"
+                    value={product.price ?? ''}
+                    placeholder='가격'
+                    required
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    name="category"
+                    value={product.category ?? ''}
+                    placeholder='카테고리'
+                    required
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    name="description"
+                    value={product.description ?? ''}
+                    placeholder='제품 설명'
+                    required
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    name="options"
+                    value={product.options ?? ''}
+                    placeholder='옵션들 콤마(,)로 구분'
+                    required
+                    onChange={handleChange}
+                />
+                <Button text={'제품 등록하기'}></Button>
+            </form>
+        </section>
     )
 }
 
