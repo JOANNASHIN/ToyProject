d1b6c5e004ed8653a4b39422189fd7e68188effe
joannashin <joannashin3@gmail.com>
2023-05-07 22:58:11 +0900

    상품리스트 컴포넌트

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import Products from '../components/Products';
 
 function AllProducts() {
-    return (
-        <>
-            AllProducts
-        </>
-    )
+    return <Products />
 }
 
 export default AllProducts;
