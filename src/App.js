2dfa6850c67906f7cc7745db9bf8288dbf6f57cd
joannashin <joannashin3@gmail.com>
2023-05-07 22:57:00 +0900

    react query 세팅

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import AuthContextProvider from './components/context/AuthContext.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
+const queryClient = new QueryClient();
 function App() {
   return (
-    <AuthContextProvider>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
