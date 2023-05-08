a734765209e0cca133aa735a8b68caeaf888b068
joannashin <joannashin3@gmail.com>
2023-05-08 23:32:02 +0900

    update cart ui

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,6 +3,8 @@ import { RiDeleteBin5Fill } from 'react-icons/ri';
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
 import { removeFromCart, updateToCart } from '../api/firebase';
 
+const ICON_CLASS = 'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1';
+
 export default function CartItem({ 
     product,
     product: {id, image, title, option, quantity, price},
@@ -29,17 +31,21 @@ export default function CartItem({
     }
 
     return (
-        <li>
-            <img src={image} alt={title} />
-            <div>
-                <p>{title}</p>
-                <p>{option}</p>
-
-                <div>
-                    <AiOutlineMinusSquare onClick={handleMinus} />
+        <li className='flex justify-between my-2 items-center'>
+            <figure className='w-24 md:w-48 mr-4'>
+                <img className='rounded-lg' src={image} alt={title} />
+            </figure>
+            <div className='flex flex-1 justify-between'>
+                <div className='basis-3/5'>
+                    <p className='text-lg'>{title}</p>
+                    <p className='text-xl font-bold text-brand'>{option}</p>
+                    <p>₩{price}</p>
+                </div>
+                <div className='text-2xl flex items-center'>
+                    <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
                     <span>{quantity}</span>
-                    <AiOutlinePlusSquare onClick={handlePlus}/>
-                    <RiDeleteBin5Fill onClick={handleDelete}/>
+                    <AiOutlinePlusSquare className={ICON_CLASS} onClick={handlePlus}/>
+                    <RiDeleteBin5Fill className={ICON_CLASS} onClick={handleDelete}/>
                 </div>
             </div>
         </li>
