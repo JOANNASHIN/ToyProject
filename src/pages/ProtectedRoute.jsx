ddab21fc587a1b123a570d57b000b43ab535d498
joannashin <joannashin3@gmail.com>
2023-05-14 02:43:15 +0900

    권한 관련 오류 수정 (유저체크전로직추가)

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -4,11 +4,15 @@ import { useAuthContext } from '../context/AuthContext';
 
 export default function ProtectedRoute({children, requireAdmin}) {
     const { user } = useAuthContext();
+    console.log('여기서 체크')
+
+    // 체크 전
+    if (user === undefined) return;
 
     // 비로그인 유저거나 어드민권한이 있는곳에 어드민 권한이 없는 경우 홈으로 리다이렉트
     if (!user || (!user.isAdmin && requireAdmin)) {
         return <Navigate to="/" replace></Navigate>
-    }
+    } 
 
     return children;
 }
